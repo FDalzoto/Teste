093380695f272350ec19a4376d7fa3963c02be4c
Felipe <20251ctb0100036@estudantes.ifpr.edu.br>
2026-05-01 01:19:28 +0000

    alteração1

--- conexao.php
+++ conexao.php
@@ -2,9 +2,9 @@
 
 mysqli_report(MYSQLI_REPORT_OFF);
 
-$conn = mysqil_connect($host = '127.0.0.1', $dbname = 'empresa', $user = 'root');
+$con = mysqil_connect($host = '127.0.0.1', $dbname = 'empresa', $user = 'root');
 
-if(!$conn){
+if(!$con){
     echo "Verifique o usuário, senha e o nome do BD" .
     'falha ao conectar: ' . mysqil_connect_error();
     }else{
