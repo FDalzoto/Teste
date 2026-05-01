5b0d24c2f1e4b65691b31278c3a4f7bb708a72e4
Felipe <20251ctb0100036@estudantes.ifpr.edu.br>
2026-05-01 00:49:37 +0000

    teste3

---
+++ inserirFuncionario.php
@@ -0,0 +1,25 @@
+<?php
+require 'conexao.php';
+
+$vmatricula = $_POST["lmatricula"];
+$vnome = $_POST["lnome"];
+$vdataNasc = $_POST["ldataNasc"];
+$vendereco = $_POST["lendereco"];
+$vsexo = $_POST["lsexo"];
+$vsalario = $_POST["lsalario"];
+$vsupervisor = $_POST["lsupervisor"];
+$vdepto = $_POST["ldepto"];
+
+$sqliIserir = "insert into funcionario (matricula, nome, dataNasc, endereco, sexo, salario, supervisor, depto) values ('$vmatricula', '$vnome', '$vdataNasc', '$endereco', '$vsexo', '$vsalario', '$vsupervisor', '$vdepto')";
+$result = mysqli_query($con, $sqliIserir);
+
+if($result == true){
+    echo "O funcionario: " . $vnome . "foi cadastrado com sucesso.";
+} else {
+    echo "Falha no cadastro.";
+}
+?>
+
+<p>
+<a href="ListarAlterarExcluirFuncionarios.php"> Voltar para a lista de funcionarios</a>
+</p>
