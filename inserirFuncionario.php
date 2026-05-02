987083e6c248e1b131534f258c12237ccf0834b8
Felipe <20251ctb0100036@estudantes.ifpr.edu.br>
2026-05-02 00:29:04 +0000

    conexao3

--- inserirFuncionario.php
+++ inserirFuncionario.php
@@ -5,38 +5,45 @@ $banco = "empresa";
 $usuario = "root";
 $senha = "";
 
-try{
-    $con = new PDO("mysql:host=$host; dbname=$banco; charset=utf8", $usuario, $senha);  
-
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+try {
+    $con = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Conexão realizada com sucesso!";
+    
+    $vmatricula = $_POST["lmatricula"];
+    $vnome = $_POST["lnome"];
+    $vdataNasc = $_POST["ldataNasc"];
+    $vendereco = $_POST["lendereco"];
+    $vsexo = $_POST["lsexo"];
+    $vsalario = $_POST["lsalario"];
+    $vsupervisor = $_POST["lsupervisor"];
+    $vdepto = $_POST["ldepto"];
+
+    $sql = "INSERT INTO funcionario 
+    (matricula, nome, dataNasc, endereco, sexo, salario, supervisor, depto) 
+    VALUES (:matricula, :nome, :dataNasc, :endereco, :sexo, :salario, :supervisor, :depto)";
+
+    $stmt = $con->prepare($sql);
+
+    $stmt->execute([
+        ":matricula" => $vmatricula,
+        ":nome" => $vnome,
+        ":dataNasc" => $vdataNasc,
+        ":endereco" => $vendereco,
+        ":sexo" => $vsexo,
+        ":salario" => $vsalario,
+        ":supervisor" => $vsupervisor,
+        ":depto" => $vdepto
+    ]);
+
+    echo "O funcionário: " . $vnome . " foi cadastrado com sucesso.";
 
 } catch (PDOException $erro) {
-    echo "Erro na conexão: " .
-$erro->getMessage();
-
-}
-
-$vmatricula = $_POST["lmatricula"];
-$vnome = $_POST["lnome"];
-$vdataNasc = $_POST["ldataNasc"];
-$vendereco = $_POST["lendereco"];
-$vsexo = $_POST["lsexo"];
-$vsalario = $_POST["lsalario"];
-$vsupervisor = $_POST["lsupervisor"];
-$vdepto = $_POST["ldepto"];
-
-$sqliIserir = "insert into funcionario (matricula, nome, dataNasc, endereco, sexo, salario, supervisor, depto) values ('$vmatricula', '$vnome', '$vdataNasc', '$endereco', '$vsexo', '$vsalario', '$vsupervisor', '$vdepto')";
-$result = mysqli_query($con, $sqliIserir);
-
-if($result == true){
-    echo "O funcionario: " . $vnome . "foi cadastrado com sucesso.";
-} else {
-    echo "Falha no cadastro.";
+    echo "Erro: " . $erro->getMessage();
 }
 ?>
 
 <p>
-<a href="ListarAlterarExcluirFuncionarios.php"> Voltar para a lista de funcionarios</a>
+<a href="ListarAlterarExcluirFuncionarios.php">Voltar para a lista de funcionários</a>
 </p>
