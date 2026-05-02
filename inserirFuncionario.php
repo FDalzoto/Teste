4127a8a2030fd87443c9671acf776a28ad2b8960
Felipe <20251ctb0100036@estudantes.ifpr.edu.br>
2026-05-02 00:19:57 +0000

    conexao1

--- inserirFuncionario.php
+++ inserirFuncionario.php
@@ -1,5 +1,22 @@
 <?php
-require 'conexao.php';
+
+$host = "localhost";
+$banco = "empresa";
+$usuario = "root";
+$senha = "";
+
+try{
+    $conexao = new PDO("mysql:host=$host; dbname=$banco; charset=utf8", $usuario, $senha);  
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+
+    echo "Conexão realizada com sucesso!";
+
+} catch (PDOException $erro) {
+    echo "Erro na conexão: " .
+$erro->getMessage();
+
+}
 
 $vmatricula = $_POST["lmatricula"];
 $vnome = $_POST["lnome"];
